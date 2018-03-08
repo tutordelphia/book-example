bdd6d9325a3347f3b965b03882582f5e9286e7f1
Adam Levin <adamlevin@tutordelphia.com>
2018-03-08 17:40:34 -0500

    new url and view for adding to lists

--- lists/views.py
+++ lists/views.py
@@ -5,14 +5,20 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-def view_list(request):
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
     view_data = {
-        'items': Item.objects.all()
+        'list': list_
     }
     return render(request, 'list.html', view_data)
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect(f'/lists/{list_.id}/')
 
 def new_list(request):
     list_ = List.objects.create()
 
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/only-list/')
+    return redirect(f'/lists/{list_.id}/')
