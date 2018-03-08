fd432aaa4b8e771d411126b239506a72f82824cd
Adam Levin <adamlevin@tutordelphia.com>
2018-03-07 20:51:20 -0500

    adds model for list items

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from lists.models import Item
 
 class HomePageTest(TestCase):
 
@@ -12,3 +12,22 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data=data)
         self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_text = 'The first (ever) list item'
+        first_item.text = first_text
+        first_item.save()
+
+        second_item = Item()
+        second_text = "Item the second"
+        second_item.text = second_text
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        self.assertEqual(saved_items[0].text, first_text)
+        self.assertEqual(saved_items[1].text, second_text)
