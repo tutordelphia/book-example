fc588ad6e283ea057ad6aae680047f1bae8473c0
Adam Levin <adamlevin@tutordelphia.com>
2018-03-08 16:21:15 -0500

    adds new url for new lists

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 class HomePageTest(TestCase):
 
@@ -7,34 +7,25 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
-    def test_can_save_a_POST_request(self):
-        first_item_text = 'A new list item'
-        data = {'item_text': first_item_text}
-
-        response = self.client.post('/', data=data)
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, first_item_text)
-
-    def test_redirects_after_POST(self):
-        first_item_text = 'A new list item'
-        data = {'item_text': first_item_text}
-        response = self.client.post('/', data=data)
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/only-list/')
-
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
         first_item = Item()
         first_text = 'The first (ever) list item'
         first_item.text = first_text
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_text = "Item the second"
         second_item.text = second_text
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
@@ -42,10 +33,32 @@ class ItemModelTest(TestCase):
         self.assertEqual(saved_items[0].text, first_text)
         self.assertEqual(saved_items[1].text, second_text)
 
+        self.assertEqual(saved_items[0].list, list_)
+        self.assertEqual(saved_items[1].list, list_)
+
+
+class NewListTest(TestCase):
+    def test_can_save_a_POST_request(self):
+        first_item_text = 'A new list item'
+        data = {'item_text': first_item_text}
+
+        response = self.client.post('/lists/new', data=data)
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, first_item_text)
+
+    def test_redirects_after_POST(self):
+        first_item_text = 'A new list item'
+        data = {'item_text': first_item_text}
+        response = self.client.post('/lists/new', data=data)
+        self.assertRedirects(response, '/lists/only-list/')
+
+
 class ListViewTest(TestCase):
     def test_displays_all_items(self):
-        Item.objects.create(text='itemey 1')
-        Item.objects.create(text='itemey 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
         response = self.client.get('/lists/only-list/')
 
--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,8 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/only-list/')
-
     return render(request, 'home.html')
 
 def view_list(request):
@@ -14,3 +10,9 @@ def view_list(request):
         'items': Item.objects.all()
     }
     return render(request, 'list.html', view_data)
+
+def new_list(request):
+    list_ = List.objects.create()
+
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/only-list/')
