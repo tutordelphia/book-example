4d824c07f0af9d11bf620a96f6bf09e5fc1fcfef
Adam Levin <adamlevin@tutordelphia.com>
2018-03-07 22:49:04 -0500

    makes function_tests use a test server

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,12 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+# TODO Clean up after func tests run
+# TODO Remove time.sleeps
+
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -19,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8888')
+        self.browser.get(self.live_server_url)
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -52,6 +55,7 @@ class NewVisitorTest(unittest.TestCase):
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very
         # methodical)
+        # TODO finish the test
         self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
@@ -64,6 +68,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # Satisfied, she goes back to sleep
 
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
